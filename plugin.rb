030370995ff309de1fb29d47712d0a5a1155ab09
Sean Gaulin <Orderchaos@users.noreply.github.com>
2017-04-18 16:12:14 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,3 @@ register_asset "stylesheets/feature-voting.scss"
 
 enabled_site_setting :voting_enabled
 
-Discourse.top_menu_items.push(:votes)
-Discourse.anonymous_top_menu_items.push(:votes)
-Discourse.filters.push(:votes)
-Discourse.anonymous_filters.push(:votes)
